0cdde028c3af2c86cb5651c73ad8ecb4cadcbdb4
giamp109 <95345801+giamp109@users.noreply.github.com>
2023-05-15 10:37:43 +0200

    files update

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/catalogo', function () {
+    return view('catalogo') ;
+})-> name('catalogo');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/index', function () {
+    return view('index');
+})-> name('index');
